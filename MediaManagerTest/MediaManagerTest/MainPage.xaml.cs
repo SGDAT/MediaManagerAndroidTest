9e8e57389623f99b00597affa3f3893208c8c44b
Samson Gabriels <contact@samsongabriels.co.uk>
2018-10-06 23:45:55 +0100

    Add project files.

---
+++ MediaManagerTest/MediaManagerTest/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Plugin.MediaManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MediaManagerTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+            await CrossMediaManager.Current.Play("http://samsongabriels.co.uk/mpthreetest/test.mp3", Plugin.MediaManager.Abstractions.Enums.MediaFileType.Audio);
+
+            CrossMediaManager.Current.AudioPlayer.PlayingChanged += AudioPlayer_PlayingChanged;
+        }
+
+        private void AudioPlayer_PlayingChanged(object sender, Plugin.MediaManager.Abstractions.EventArguments.PlayingChangedEventArgs e)
+        {
+            Console.WriteLine(e.Position);
+        }
+    }
+}
